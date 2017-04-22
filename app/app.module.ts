1fa2c8f1d867bbf58e6e32504bfd8b2efe17e623
VerDan777 <vereshhagin98@mail.ru>
2017-04-22 14:01:40 +0300

    update forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'; 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {TodoFormComponent} from './todo-form/todo-form.component';
+// import {TodoFormComponent} from './todo-form/todo-form.component';
 
 @NgModule({
-    imports:[BrowserModule,FormsModule,TodoFormComponent],
+    imports:[BrowserModule,FormsModule],
     declarations:[AppComponent],
     bootstrap:[AppComponent]
 }) 
